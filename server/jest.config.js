25c43adb6258080d8efceb7ea770864aa3f62659
Eduardo <rochaeduardo997@protonmail.com>
2023-07-06 18:11:07 -0300

    updating jest config and gitignore

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,7 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  collectCoverageFrom: ["src/*/.ts", "!*/node_modules/", "!src/main.ts", "!src/infra/database/migrations/*"],
-  coverageReporters: ["html", "text", "text-summary", "cobertura"]
+  testEnvironment: 'node'
 };
