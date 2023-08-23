c7bcc434bbf3f96feb47786659ff3f7c43b6131c
Eduardo <rochaeduardo997@protonmail.com>
2023-08-23 20:10:29 -0300

    training sheet

--- server/src/domain/entity/workout/Sheet/TrainingSheet.ts
+++ server/src/domain/entity/workout/Sheet/TrainingSheet.ts
@@ -1,12 +1,46 @@
 import DateRegisters from "../../common/DateRegisters";
+import Athlete from "../../users/Athlete";
+import Trainer from "../../users/Trainer";
 import DayTraining from "./DayTraining";
 
 class TrainingSheet {
   constructor(
-    private id: number,
-    private dayTraining: DayTraining[],
-    private dateRegisters: DateRegisters
+    private _id:            number,
+    private _trainer:       Trainer,
+    private _athlete:       Athlete,
+    private _dayTrainings:  DayTraining[],
+    private _dateRegisters: DateRegisters,
+    private _when_change:   Date
   ){}
+
+  public get id(): number {
+    return this._id;
+  }
+  public get trainer(): Trainer {
+    return this._trainer;
+  }
+  public get athlete(): Athlete {
+    return this._athlete;
+  }
+  public get dayTrainings(): DayTraining[] {
+    return this._dayTrainings;
+  }
+  public get dateRegisters(): DateRegisters {
+    return this._dateRegisters;
+  }
+  public get when_change(): Date {
+    return this._when_change;
+  }
+
+  public set id(value: number) {
+    this._id = value;
+  }
+  public set dateRegisters(value: DateRegisters) {
+    this._dateRegisters = value;
+  }
+  public set when_change(value: Date) {
+    this._when_change = value;
+  }
 }
 
 export default TrainingSheet;
--- server/test/unit/workout/Sheet/TrainingSheet.test.ts
+++ server/test/unit/workout/Sheet/TrainingSheet.test.ts
@@ -5,23 +5,33 @@ import { generateExercise } from "../../../seeds/workout/exercise";
 import { generateTrainer } from "../../../seeds/user";
 import { generateDayTraining } from "../../../seeds/workout/sheet";
 import TrainingSheet from "../../../../src/domain/entity/workout/Sheet/TrainingSheet";
+import Trainer from "../../../../src/domain/entity/users/Trainer";
+import Athlete from "../../../../src/domain/entity/users/Athlete";
 
 let trainingSheet: TrainingSheet;
 let dateRegisters: DateRegisters
 let dayTraining1: DayTraining;
 let dayTraining2: DayTraining;
+let trainer: Trainer;
+let athlete: Athlete;
 
 beforeAll(() => {
-  const trainer  = generateTrainer(1);
+  trainer  = generateTrainer(1);
   const exercise1 = generateExercise(1, trainer);
   const exercise2 = generateExercise(1, trainer);
   dayTraining1 = generateDayTraining(1, [ exercise1 ]);
   dayTraining2 = generateDayTraining(2, [ exercise1, exercise2 ]);
   dateRegisters = generateDateRegisters(1);
-  trainingSheet = new TrainingSheet(1, [ dayTraining1, dayTraining2 ], dateRegisters);
+  trainingSheet = new TrainingSheet(1, trainer, athlete, [ dayTraining1, dayTraining2 ], dateRegisters, new Date('2024-01-01'));
 });
 
 describe('Successful cases', () =>{
   test('Normal training sheet', () => {
+    expect(trainingSheet.id).toBe(1);
+    expect(trainingSheet.trainer).toEqual(trainer);
+    expect(trainingSheet.athlete).toEqual(athlete);
+    expect(trainingSheet.dayTrainings).toEqual([ dayTraining1, dayTraining2 ]);
+    expect(trainingSheet.dateRegisters).toEqual(dateRegisters);
+    expect(trainingSheet.when_change).toEqual(new Date('2024-01-01'));
   });
 });
