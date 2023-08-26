12e4ad58a595999bf29cf70ffd10b9138361bfd0
Eduardo <rochaeduardo997@protonmail.com>
2023-08-26 15:32:28 -0300

    adding training sheet

--- server/src/domain/entity/workout/Sheet/TrainingSheet.ts
+++ server/src/domain/entity/workout/Sheet/TrainingSheet.ts
@@ -9,9 +9,44 @@ class TrainingSheet {
     private _trainer:       Trainer,
     private _athlete:       Athlete,
     private _dayTrainings:  DayTraining[],
-    private _dateRegisters: DateRegisters,
-    private _when_change:   Date
+    private _dateRegisters: DateRegisters = new DateRegisters(),
+    private _when_change?:  Date
   ){}
+
+  public update(input: TUpdateInput): boolean{
+    if(input.when_change) this.when_change = input.when_change;
+
+    this.updated_at = new Date();
+
+    return true;
+  }
+
+  public addDayTraining(x: DayTraining): boolean{
+    this.alreadyExists(x);
+    this.dayTrainings.push(x);
+
+    return true;
+  }
+  private alreadyExists(x: DayTraining): void{
+    const has = this.dayTrainings.find((dt: DayTraining) => {
+      const hasOnWeek = dt.week === x.week;
+      const hasOnDay  = dt.day  === x.day;
+      console.log(dt.week, dt.day);
+      console.log(x.week, x.day);
+      return hasOnWeek && hasOnDay;
+    });
+    if(has) throw new Error('Already exists a training on this day at this week');
+    return;
+  }
+
+  public removeDayTraining(x: DayTraining): boolean{
+    const index = this.dayTrainings.findIndex((dt: DayTraining) => dt.id === x.id);
+    if(index < 0) throw new Error('Training doesn\'t exists');
+
+    this.dayTrainings.splice(index, 1);
+
+    return true;
+  }
 
   public get id(): number {
     return this._id;
@@ -25,22 +60,29 @@ class TrainingSheet {
   public get dayTrainings(): DayTraining[] {
     return this._dayTrainings;
   }
-  public get dateRegisters(): DateRegisters {
-    return this._dateRegisters;
+  public get created_at(): Date {
+    return this._dateRegisters.created_at;
   }
-  public get when_change(): Date {
+  public get updated_at(): Date {
+    return this._dateRegisters.updated_at;
+  }
+  public get deleted_at(): Date | undefined {
+    return this._dateRegisters.deleted_at;
+  }
+  public get when_change(): Date | undefined {
     return this._when_change;
   }
 
-  public set id(value: number) {
-    this._id = value;
-  }
-  public set dateRegisters(value: DateRegisters) {
-    this._dateRegisters = value;
+  public set updated_at(value: Date) {
+    this._dateRegisters.updated_at = value;
   }
   public set when_change(value: Date) {
     this._when_change = value;
   }
 }
+
+type TUpdateInput = { 
+  when_change?: Date;
+};
 
 export default TrainingSheet;
--- server/test/unit/workout/Sheet/TrainingSheet.test.ts
+++ server/test/unit/workout/Sheet/TrainingSheet.test.ts
@@ -15,12 +15,12 @@ let dayTraining2: DayTraining;
 let trainer: Trainer;
 let athlete: Athlete;
 
-beforeAll(() => {
+beforeEach(() => {
   trainer  = generateTrainer(1);
   const exercise1 = generateExercise(1, trainer);
   const exercise2 = generateExercise(1, trainer);
-  dayTraining1 = generateDayTraining(1, [ exercise1 ]);
-  dayTraining2 = generateDayTraining(2, [ exercise1, exercise2 ]);
+  dayTraining1 = generateDayTraining(1, 1, [ exercise1 ]);
+  dayTraining2 = generateDayTraining(2, 2, [ exercise1, exercise2 ]);
   dateRegisters = generateDateRegisters(1);
   trainingSheet = new TrainingSheet(1, trainer, athlete, [ dayTraining1, dayTraining2 ], dateRegisters, new Date('2024-01-01'));
 });
@@ -31,7 +31,55 @@ describe('Successful cases', () =>{
     expect(trainingSheet.trainer).toEqual(trainer);
     expect(trainingSheet.athlete).toEqual(athlete);
     expect(trainingSheet.dayTrainings).toEqual([ dayTraining1, dayTraining2 ]);
-    expect(trainingSheet.dateRegisters).toEqual(dateRegisters);
+    expect(trainingSheet.created_at).toEqual(dateRegisters.created_at);
+    expect(trainingSheet.updated_at).toEqual(dateRegisters.updated_at);
+    expect(trainingSheet.deleted_at).toEqual(dateRegisters.deleted_at);
     expect(trainingSheet.when_change).toEqual(new Date('2024-01-01'));
+  });
+  test('Adding day training to sheet', () => {
+    const exercise     = generateExercise(3, trainer);
+    const dayTraining3 = generateDayTraining(3, 3, [ exercise ]);
+
+    trainingSheet.addDayTraining(dayTraining3);
+
+    expect(trainingSheet.dayTrainings).toEqual([ dayTraining1, dayTraining2, dayTraining3 ]);
+  });
+  test('Adding day training to sheet', () => {
+    const exercise     = generateExercise(3, trainer);
+    const dayTraining3 = generateDayTraining(3, 3, [ exercise ]);
+
+    trainingSheet.addDayTraining(dayTraining3);
+    trainingSheet.removeDayTraining(dayTraining2);
+
+    expect(trainingSheet.dayTrainings).toEqual([ dayTraining1, dayTraining3 ]);
+  });
+
+  test('Update training sheet', () => {
+    trainingSheet.update({ when_change: new Date('2024-02-02') });
+
+    expect(trainingSheet.id).toBe(1);
+    expect(trainingSheet.trainer).toEqual(trainer);
+    expect(trainingSheet.athlete).toEqual(athlete);
+    expect(trainingSheet.dayTrainings).toEqual([ dayTraining1, dayTraining2 ]);
+    expect(trainingSheet.created_at).toEqual(dateRegisters.created_at);
+    expect(trainingSheet.updated_at).toEqual(dateRegisters.updated_at);
+    expect(trainingSheet.deleted_at).toEqual(dateRegisters.deleted_at);
+    expect(trainingSheet.when_change).toEqual(new Date('2024-02-02'));
+  });
+});
+
+describe('Failure cases', () =>{
+  test('Fail on add a training with week and day that already exists', () => {
+    const exercise     = generateExercise(3, trainer);
+    const dayTraining3 = generateDayTraining(3, 3, [ exercise ]);
+    trainingSheet.addDayTraining(dayTraining3);
+
+    expect(() => trainingSheet.addDayTraining(dayTraining3)).toThrow('Already exists a training on this day at this week');
+  });
+  test('Fail on remove a training that doesnt exists', () => {
+    const exercise     = generateExercise(3, trainer);
+    const dayTraining3 = generateDayTraining(3, 3, [ exercise ]);
+
+    expect(() => trainingSheet.removeDayTraining(dayTraining3)).toThrow('Training doesn\'t exists');
   });
 });
