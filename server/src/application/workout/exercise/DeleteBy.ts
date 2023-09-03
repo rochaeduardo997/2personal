f6601b42422201a91cc26d9dc3f43bca0c3f9f27
Eduardo <rochaeduardo997@protonmail.com>
2023-09-03 10:03:52 -0300

    removing useless user repository from exercise delete application

--- server/src/application/workout/exercise/DeleteBy.ts
+++ server/src/application/workout/exercise/DeleteBy.ts
@@ -1,15 +1,9 @@
-import Trainer from "../../../domain/entity/users/Trainer";
-import IUserRepository from "../../../domain/repository/users/IUserRepository";
 import IExerciseRepository from "../../../domain/repository/workout/IExerciseRepository";
 
 class DeleteBy {
-  constructor(
-    private exerciseRepository: IExerciseRepository,
-    private userRepository:     IUserRepository
-  ){}
+  constructor(private exerciseRepository: IExerciseRepository){}
 
   async execute(input: TInput): Promise<boolean>{
-    await this.userRepository.get(input.trainer_id) as Trainer;
     const result = await this.exerciseRepository.deleteBy(input.exercise_id, input.trainer_id);
     return result;
   }
--- server/test/infra/application/workout/exercise/DeleteBy.test.ts
+++ server/test/infra/application/workout/exercise/DeleteBy.test.ts
@@ -1,14 +1,12 @@
 import DeleteBy from "../../../../../src/application/workout/exercise/DeleteBy";
 import Trainer from "../../../../../src/domain/entity/users/Trainer";
 import Exercise from "../../../../../src/domain/entity/workout/exercise/Exercise";
-import IUserRepository from "../../../../../src/domain/repository/users/IUserRepository";
 import IExerciseRepository from "../../../../../src/domain/repository/workout/IExerciseRepository";
 import RepositoryFactoryMemory from "../../../../../src/infra/factory/RepositoryFactoryMemory";
 import { generateTrainer } from "../../../../seeds/user";
 import { generateExercise } from "../../../../seeds/workout/exercise";
 
 let exerciseRepository: IExerciseRepository;
-let userRepository: IUserRepository;
 let trainer: Trainer;
 let exercise1: Exercise;
 let exercise2: Exercise;
@@ -20,9 +18,7 @@ beforeEach(async () => {
 
   const repositoryFactory = new RepositoryFactoryMemory();
   exerciseRepository = repositoryFactory.exerciseRepository();
-  userRepository = repositoryFactory.userRepository();
 
-  userRepository.save(trainer);
   exerciseRepository.save(exercise1);
   exerciseRepository.save(exercise2);
 });
@@ -33,7 +29,7 @@ describe('Successful cases', () => {
       trainer_id:  trainer.id,
       exercise_id: exercise1.id
     };
-    const deleteBy = new DeleteBy(exerciseRepository, userRepository);
+    const deleteBy = new DeleteBy(exerciseRepository);
     const result = await deleteBy.execute(input);
 
     expect(result).toBeTruthy();
